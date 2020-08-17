ea4fe502c3c5c3c8275a40a13c6cc6aeb5b13605
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-17 06:07:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,10 @@ def nyc_pigeon_organizer(data)
   final_dic = data.each_with_object({}) do |(key,value), final_ary|
     values.each do |inner key , names|
       names.each do |name|
+        if 
+        end
+        if 
+        end
         
       end
     end
