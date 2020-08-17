eaaff34b50dc9a5a32ee12b423ba5160f3ea61d8
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-17 06:13:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_dic = data.each_with_object({}) do |(key, value), final_ary|
+    value.each do |inner_key , names|
+      names.each do |name|
+        if !final_ary[name]
+          final_ary[name] = {}
+        end
+        if !final_ary[name][key]
+          !final_ary[name][key] = []
+        end
+        final_ary[name][key].push(inner_key.to_s)
+      end
+    end
+  end
+  final_dic
 end
